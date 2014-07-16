ff11a5c2eb1aaf94342b8185e27882a1b76d92b2
Faraz Masood Khan <faraz@fanaticlab.com>
2014-07-16 06:28:35 +0500

    Demo debug setting for dev machines

--- demo/settings.py
+++ demo/settings.py
@@ -9,7 +9,15 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import socket
+import os.path
+
+DEV_MACS = ('Dev-Mac', 'Envy15', 'envy', 'trg-tech-farazm', 'faraz-VirtualBox', 'trg-tech-faraz', 'macbook-pro')
+
+
+DEBUG = socket.gethostname() in DEV_MACS
+TEMPLATE_DEBUG = DEBUG
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -60,8 +68,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'doorsale',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'USER': 'doorsale',                      # Not used with sqlite3.
+        'PASSWORD': 'doorsale',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
