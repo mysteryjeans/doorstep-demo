6b510182ef3697ccbdf0ac9ca7cc8a37a97bab61
Faraz Masood Khan <faraz@fanaticlab.com>
2014-09-17 12:32:57 +0500

    Converted to sqlite database and added fixture to migrate different db

--- demo/settings.py
+++ demo/settings.py
@@ -64,14 +64,21 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'doorsale',                      # Or path to database file if using sqlite3.
+#         'USER': 'doorsale',                      # Not used with sqlite3.
+#         'PASSWORD': 'doorsale',                  # Not used with sqlite3.
+#         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'doorsale',                      # Or path to database file if using sqlite3.
-        'USER': 'doorsale',                      # Not used with sqlite3.
-        'PASSWORD': 'doorsale',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -100,38 +107,164 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Doorsale settings
-# https://github.com/mysteryjeans/doorsale
-import doorsale.settings
+FIXTURE_DIRS = (
+    os.path.join(BASE_DIR, 'fixtures'),
+)
 
+################## Doorsale #########################
+#####################################################
 
-# Authentication model settings
-AUTH_USER_MODEL = doorsale.settings.AUTH_USER_MODEL
-LOGIN_URL = doorsale.settings.LOGIN_URL
-LOGIN_REDIRECT_URL = doorsale.settings.LOGIN_REDIRECT_URL
+# Doorsale e-commerce settings for Django project
+# Customize these settings only if you know
 
+# Django authentication app extended by Doorsale in Django way
+# To implement your own custom auth user model, extend
+# doorsale.accounts.AbstractUser instead and change AUTH_USER_MODEL
+# similarly. Doorsale will adopt new AUTH_USER_MODEL seemlessly
+AUTH_USER_MODEL = 'accounts.User'
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
+# Doorsale apps configuration required for Django
+INSTALLED_APPS += (
+    'doorsale',
+    'doorsale.geo',
+    'doorsale.pages',
+    'doorsale.accounts',
+    'doorsale.catalog',
+    'doorsale.sales',
+    'doorsale.financial',
+    'doorsale.payments',
+)
+
+# DOMAIN will be used in link generation specially in emails
+DOMAIN = '127.0.0.1:8000'
+
+# SITE_NAME it will be used in all pages, this is the name of your website
+SITE_NAME = 'Doorsale'
+
+# SITE_TITLE for index pages of your website
+SITE_TITLE = 'The powerful e-commerce solution for Django'
+
+# Meta description for SEO
+SITE_DESCRIPTION = 'The e-commerce solution site build using Doorsale'
+
+# COPYRIGHT statement for all pages
+COPYRIGHT = 'Copyright &copy; 2014 Doorsale. All rights reserved.'
+
+# SUPPORT_EMAIL address for bugs and error reporting
 SUPPORT_EMAIL = 'demo@doorsaledemo.com'
 
-# Including doorsale apps
-INSTALLED_APPS += doorsale.settings.DOORSALE_APPS
 
-PIPELINE_CSS =  doorsale.settings.PIPELINE_CSS
+################ Django-Pipeline #####################
+######################################################
 
-PIPELINE_JS =  doorsale.settings.PIPELINE_JS
+# Doorsale uses django-pipeline for LESS & Javascript
+# preprocessing, compression and versioning.
 
-PIPELINE_COMPILERS = doorsale.settings.PIPELINE_COMPILERS
+# When collectstatic called during deployment LESS & Javascript will be
+# compiled, compressed and versioned before copying to static folder
 
-STATICFILES_STORAGE = doorsale.settings.STATICFILES_STORAGE
+# Django-Pipeline app required by Doorsale
+INSTALLED_APPS += ('pipeline',)
 
-PIPELINE_DISABLE_WRAPPER = doorsale.settings.PIPELINE_DISABLE_WRAPPER
+# LESS compiler configuration for django-pipeline
+PIPELINE_COMPILERS = ('pipeline.compilers.less.LessCompiler',)
 
-DOMAIN = '127.0.0.1:8000'
-SITE_NAME = 'Doorsale Demo'
-SITE_TITLE = 'The powerful e-commerce solution for Django'
-SITE_DESCRIPTION = 'The e-commerce solution demo site build using Doorsale'
-COPYRIGHT = 'Doorsale (c) 2014'
-CONTACT_EMAIL = 'demo@doorsaledemo.com'
+# LESS & Javascript static files serving in development
+STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+
+# Custom Javascript needs to be in global scope
+# in order for Doorsale to work properly
+PIPELINE_DISABLE_WRAPPER = True
+
+
+# CSS configurations for django-pipeline
+# All LESS styles configured for doorsale defined
+# You can append your LESS configurations here.
+PIPELINE_CSS = {
+    # doorsale app LESS styles
+    'base': {
+        'source_filenames': (
+            'doorsale/css/base.less',
+        ),
+        'output_filename': 'doorsale/css/base.css'
+    },
+    # doorsale.catalog LESS styles for products catalog
+    'catalog': {
+        'source_filenames': (
+            'catalog/css/catalog.less',
+        ),
+        'output_filename': 'catalog/css/catalog.css'
+    },
+    # Font-Awesome icons serve mostly
+    'font-awesome': {
+        'source_filenames': (
+            'catalog/css/font-awesome/css/font-awesome.min.css',
+        ),
+        'output_filename': 'catalog/css/font-awesome/css/font-awesome.min.css'
+    },
+    # doorsale.sales LESS styles for checkout pages
+    'sales': {
+        'source_filenames': (
+            'sales/css/sales.less',
+        ),
+        'output_filename': 'sales/css/sales.css'
+    },
+    # doorsale.pages LESS styles for flat pages
+    'pages': {
+        'source_filenames': (
+            'pages/css/pages.less',
+        ),
+        'output_filename': 'pages/css/pages.css'
+    }
+}
+
+
+# Javascript configuration for django-pipeline
+# Doorsale app's Javascript compressed & versioned before deployment
+# Simply add your project or apps Javascript here
+PIPELINE_JS = {
+    # doorsale: Base javascript include in every page
+    'base': {
+        'source_filenames': (
+            'doorsale/scripts/jquery-ajax.js',
+            'doorsale/scripts/jquery-utils.js',
+        ),
+        'output_filename': 'doorsale/scripts/base.js',
+    },
+    # doorsale.catalog: Javascript for product catalog pages
+    'catalog_base': {
+        'source_filenames': (
+            'catalog/scripts/jquery.catalog_base.js',
+        ),
+        'output_filename': 'catalog/scripts/catalog_base.js',
+    },
+    'search_products': {
+        'source_filenames': (
+            'catalog/scripts/jquery.search_products.js',
+        ),
+        'output_filename': 'catalog/scripts/search_products.js',
+    },
+    'product_detail': {
+        'source_filenames': (
+            'catalog/scripts/jquery.scrollTo.js',
+            'catalog/scripts/jquery.serialScroll.js',
+            'catalog/scripts/jquery.elevatezoom.js',
+            'catalog/scripts/jquery.product_detail.js',
+        ),
+        'output_filename': 'catalog/scripts/product_detail.js',
+    },
+    'sales_checkout_order': {
+        'source_filenames': (
+            'sales/scripts/jquery.creditCardValidator.js',
+            'sales/scripts/jquery.maskedinput.js',
+            'sales/scripts/jquery.checkout_order.js',
+        ),
+        'output_filename': 'sales/scripts/sales_checkout_order.js',
+    }
+}
+
 
 # Email server settings
 # You should define your email server settings here  
