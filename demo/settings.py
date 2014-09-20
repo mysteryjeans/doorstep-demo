a4eae45124285e12141a84e52718526924ca30ec
Faraz Masood Khan <faraz@fanaticlab.com>
2014-09-20 06:38:54 +0500

    Synchronizing site settings to database level modification

--- demo/settings.py
+++ demo/settings.py
@@ -67,9 +67,9 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'doorsale',                      # Or path to database file if using sqlite3.
-#         'USER': 'doorsale',                      # Not used with sqlite3.
-#         'PASSWORD': 'doorsale',                  # Not used with sqlite3.
+#         'NAME': 'doorsale2',                      # Or path to database file if using sqlite3.
+#         'USER': 'doorsale2',                      # Not used with sqlite3.
+#         'PASSWORD': 'doorsale2',                  # Not used with sqlite3.
 #         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
 #         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
 #     }
--- demo/urls.py
+++ demo/urls.py
@@ -2,14 +2,34 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
-
-import doorsale.urls
-
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^', include(doorsale.urls)),
-)
+    # Examples:
+    # url(r'^$', 'doorsale_site.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
+    url(r'^admin/', include(admin.site.urls)),
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+############################################
+# Doorsale urls containing urls for all apps
+urlpatterns += patterns('',
+                        url(r'^', include('doorsale.urls')),
+)
+
+################### OR #####################
+# You adjust each app urls according to your preferences
+# urlpatterns += patterns('',
+#     # Doorsale apps urls
+#     url(r'^', include('doorsale.catalog.urls')),
+#     url(r'^accounts/', include('doorsale.accounts.urls')),
+#     url(r'^sales/', include('doorsale.sales.urls')),
+#     url(r'^payments/', include('doorsale.payments.urls')),
+#     url(r'^pages/', include('doorsale.pages.urls'))
+# )
